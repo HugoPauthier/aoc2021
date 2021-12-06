557688c53082bfa002eefb21b261ad9e3c783ee7
Hugo Pauthier <hugo.pauthier@gmail.com>
2021-12-06 13:42:56 +0000

    Finally finished part 2

--- days/06/lanternfish.py
+++ days/06/lanternfish.py
@@ -1,5 +1,6 @@
 test = [3, 4, 3, 1, 2]
 
+
 def prepare_lists(input_list):
     res = dict.fromkeys([0, 1, 2, 3, 4, 5, 6, 7, 8], 0)
     for n in input_list:
@@ -11,32 +12,28 @@ def prepare_lists(input_list):
 
 def part1(init_fishes, countdown):
     fish_list = init_fishes.copy()
-    for d in range(countdown):
-        print('Day :', d)
+    for day in range(countdown):
         for i in range(len(fish_list)):
             if fish_list[i] == 0:
                 fish_list.append(8)
                 fish_list[i] = 7
             fish_list[i] += -1
-
         # print(fish_list)
     return len(fish_list)
 
-# print(part1(test, 18))
-# assert part1(test, 80) == 5934
-
-    
 
 def part2(dict_fishes, countdown):
     for day in range(countdown):
-        print(dict_fishes)
-        day_dict = dict_fishes.copy()
-        for k, v in day_dict.items():
-            if v > 0:
-                print(k)
+        day_dict = dict.fromkeys([0, 1, 2, 3, 4, 5, 6, 7, 8], 0)
+        fishes0 = dict_fishes[0]
+        day_dict[8] = fishes0
+        day_dict[6] = fishes0
+        for k in dict_fishes.keys():
+            if k != 0:
+                if k == 7:
+                    day_dict[6] = dict_fishes[k] + day_dict[6]
+                else:
+                    day_dict[k-1] = dict_fishes[k]
         dict_fishes = day_dict
+        # print('end   day:', day + 1, '=>', dict_fishes)
     return sum(dict_fishes.values())
-
-dict_test = prepare_lists(test)
-print(part2(dict_test, 2))
-# print(part2(test, 250))
--- days/06/main.py
+++ days/06/main.py
@@ -1,4 +1,5 @@
-from part_one import *
+from lanternfish import *
+
 
 def prepare_data(file):
     """It prepare the fishes list"""
@@ -7,7 +8,13 @@ def prepare_data(file):
         fishes_list = [int(i) for i in fishes[0].split(',')]
     return fishes_list
 
+
 d0_fishes_list = prepare_data('input.txt')
-# d80_fishes = part1(d0_fishes_list, 80)
-d256_fishes = part1(d0_fishes_list, 256)
-# print(d80_fishes)
+
+
+print(part1(d0_fishes_list, 80))  # 355386
+assert part1(d0_fishes_list, 80) == 355386
+
+d0_fishes_dict = prepare_lists(d0_fishes_list)
+print(part2(d0_fishes_dict, 256))
+assert part2(d0_fishes_dict, 256) == 1613415325809
