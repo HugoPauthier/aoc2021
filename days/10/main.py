da73b2839feb5e5aa808dfca6159f4791b78791e
HugoPauthier <hugo.pauthier@gmail.com>
2021-12-10 12:05:33 +0000

    Day 10 - part 2

--- days/10/main.py
+++ days/10/main.py
@@ -1,14 +1,16 @@
-from part_one import *
+from nav_subsystem import *
 
 with open('test_input.txt') as file:
     test_nav_sub = file.read().splitlines()
     # print(part1(test_nav_sub))
-    # assert part1(test_nav_sub) == 26397
+    assert part1(test_nav_sub) == 26397
     # print(part2(test_nav_sub))
+    assert part2(test_nav_sub) == 288957
 
 
 with open('input.txt') as file:
     nav_dub = file.read().splitlines()
     # print(part1(nav_dub))
-    # assert part1(nav_dub) == 464991
-    print(part2(nav_dub))
+    assert part1(nav_dub) == 464991
+    # print(part2(nav_dub))
+    assert part2(nav_dub) == 3662008566
